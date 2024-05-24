3536dd3691ceb8c3fd7b2d3e7ac97cade962605b
Geovanne Perez <geovanne.perez@outlook.com>
2024-05-24 10:18:09 -0600

    Agregado mongoose y conexión a DB

---
+++ database/config.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Conexión a base de datos realizada con éxito");
+} catch (error) {
+    console.log(error);
+    throw new error("Error abriendo conexión con MongoDB")
+}
+}
+
+module.exports = {
+    dbConnection
+};
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+// Invocar express
 const express = require('express')
+
+// Llamar dotenv (requiere NPM install) para obtener variables de entorno
+require('dotenv').config()
+
 const cors = require('cors')
 const swaggerUI = require('swagger-ui-express')
 const swaggerJsDoc = require('swagger-jsdoc')
 const path = require('path');
 const { MongoClient, ObjectId } = require('mongodb'); 
 
-require('dotenv').config()
+// Obtener Puerto desde environment
 const port = process.env.PORT 
 console.log(process.env)
+
 const serverUrl = process.env.SERVER_URL || `http://localhost:${port}`
 
 const swaggerOptions = {
@@ -25,7 +31,6 @@ const swaggerOptions = {
     },
     apis: [`${path.join(__dirname, './routes/*.js')}`],
 }
-
 const swaggerDocs = swaggerJsDoc(swaggerOptions)
 
 const app = express();
@@ -37,18 +42,22 @@ app.get('/',(req,res) => {
 app.use(cors());
 app.use(express.json());
 
+
 // Llamada de Ruta como app.use sencillo
-app.use('/api/users', require('./routes/user.Routes.js'))
+    //app.use('/api/users', require('./routes/user.Routes.js'))
 
 // También se puede usar como IIFE
-// IIFE = Immediately Invoked Function Expression
-/*
-const userRoutes =  require('./routes/user.Routes.js')
-(()=>{
-app.use(userRoutes)
+    // IIFE = Immediately Invoked Function Expression
+
+const userRoutes =  require('./routes/user.Routes');
+const {dbConnection} = require('./database/config.js');
+
+(async ()=> {
+    await dbConnection();
+    app.use('/api/users',userRoutes)
 })();
-*/
+
 // Use Swagger
-app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+//app.use('/api/', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 app.listen(port, () => {console.log(`Web server iniciado en http://localhost:${port}/`);});
