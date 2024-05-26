8fa9f2e153248a066dbb68b39c65538b51d8de79
Geovanne Perez <geovanne.perez@outlook.com>
2024-05-25 23:28:15 -0600

    Corregida creación de usuarios con Password Hasheado y validación de Username y Password no vacíos

--- controllers/users.Controller.js
+++ controllers/users.Controller.js
@@ -1,6 +1,8 @@
 //const database = dbClient.db('DWFS_P6');
 const {response,request} = require('express');
 const UserModel = require('../models/user.Model');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Sample routes for testing
 // Se coloca el req = request para que el compilador identifique el tipo de dato, y se visualice el autocompletado
@@ -59,18 +61,30 @@ const userGetById = (async (req = request,res = response) => {
 });
 
 const userCreate = (async (req = request,res = response) => {
-	const {Username,Password,Name,Lastname,Email,Enabled}= req.body;
 
 	try {
-		 // Check if username already exists in the database
-		 const existingUser = await UserModel.findOne({ Username: Username });
+		
+		const {Username,Password,Name,Lastname,Email,Enabled}= req.body;
+		let user = new UserModel({Username,Password,Name,Lastname,Email,Enabled});
+		
+		console.log("Username: ",user.Username);
+		console.log("Password: ",user.Password);
 
+		if (Username == null || Password == null) {
+			// Username or Password Empty (400 Conflict)
+			return res.status(400).send({ message: "Se requiere Username y Password" });
+		}
+
+		 // Check if username already exists in the database
+		 const existingUser = await UserModel.findOne({ Username: user.Username});
+
+		 console.log("existingUser: ",existingUser);
 		 if (existingUser) {
 		   // Username conflict (409 Conflict)
 		   return res.status(409).send({ message: "Nombre de usuario ya existe" });
 		 }
 		 // Check if Email already exists in the database
-		 const existingEmail = await UserModel.findOne({ Email: Email});
+		 const existingEmail = await UserModel.findOne({ Email: user.Email});
 
 		 if (existingEmail) {
 		   // Username conflict (409 Conflict)
@@ -78,7 +92,10 @@ const userCreate = (async (req = request,res = response) => {
 		 }
 		 
     	// Username doesn't exist, proceed with user creation
-		let user = new UserModel({Username,Password,Name,Lastname,Email,Enabled});
+		//Hash password
+		const salt = await bcryptjs.genSalt(10);
+		const hashedPassword = await bcryptjs.hash(user.Password,salt);
+		user.Password = hashedPassword;
 		const createdUser = await user.save();
 		res.status(200).json({
 			message:"Usuario creado correctamente",
@@ -105,16 +122,17 @@ const userUpdate = (async (req = request,res = response) => {
 		}
 		else{
 			
+		 const {Username,Name,Lastname,Email,Enabled}= req.body;
+		 let newUser = new UserModel({Username,Password,Name,Lastname,Email,Enabled});
 		
 		 // Check if username exists in the database
-		 const existingUser = await UserModel.findOne({ Username: req.body.username });
+		 var existingUser = await UserModel.findOne({ Username: newUser.username });
 		
 		 
 		// Username exists, proceed with user update
 		 if (existingUser) {
-			const {Username,Password,Name,Lastname,Email,Enabled}= req.body;
-			let _user = new UserModel({_id:id,Username,Password,Name,Lastname,Email,Enabled});
-			const updatedUser = await UserModel.findByIdAndUpdate(id,_user,{new:true});
+			existingUser = {Username,Name,Lastname,Email,Enabled};
+			const updatedUser = await UserModel.findByIdAndUpdate(id,existingUser,{new:true});
 			res.status(200).json({
 				message:"Usuario actualizado correctamente",
 				data: updatedUser
--- models/user.Model.js
+++ models/user.Model.js
@@ -3,7 +3,7 @@ const {Schema,model} = require("mongoose");
 const userSchema = Schema({
     Username: {
         type: String,
-        required:true [true,'El user es obligatorio'],
+        required: true [true,'El user es obligatorio'],
         unique: true,
     },
     Password: {
