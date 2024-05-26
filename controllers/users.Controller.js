d9890371b6b389b9a09950e8c852c8066da5f9f4
Geovanne Perez <geovanne.perez@outlook.com>
2024-05-25 22:48:35 -0600

    Agregada desestructuración de req.body en variables independientes, para mejorar la seguridad

--- controllers/users.Controller.js
+++ controllers/users.Controller.js
@@ -38,10 +38,16 @@ const userGetById = (async (req = request,res = response) => {
 		}
 		else{
 			const user = await UserModel.findById(id);
-			res.status(200).json({
-				message:"Usuario encontrado",
-				data: user
-			});
+			if (user != null) {
+				console.log(user);
+				res.status(200).json({
+					message:"Usuario encontrado",
+					data: user
+				});
+			} else {
+				return res.status(409).send({ message: "Usuario no encontrado" });
+				
+			}
 		}
 	} catch (error) {
 		console.error("Error interno buscando el usuario:", error.message);
@@ -53,17 +59,18 @@ const userGetById = (async (req = request,res = response) => {
 });
 
 const userCreate = (async (req = request,res = response) => {
-	const body = req.body;
+	const {Username,Password,Name,Lastname,Email,Enabled}= req.body;
+
 	try {
 		 // Check if username already exists in the database
-		 const existingUser = await UserModel.findOne({ Username: body.Username });
+		 const existingUser = await UserModel.findOne({ Username: Username });
 
 		 if (existingUser) {
 		   // Username conflict (409 Conflict)
 		   return res.status(409).send({ message: "Nombre de usuario ya existe" });
 		 }
 		 // Check if Email already exists in the database
-		 const existingEmail = await UserModel.findOne({ Email: body.Email});
+		 const existingEmail = await UserModel.findOne({ Email: Email});
 
 		 if (existingEmail) {
 		   // Username conflict (409 Conflict)
@@ -71,7 +78,7 @@ const userCreate = (async (req = request,res = response) => {
 		 }
 		 
     	// Username doesn't exist, proceed with user creation
-		let user = new UserModel(body);
+		let user = new UserModel({Username,Password,Name,Lastname,Email,Enabled});
 		const createdUser = await user.save();
 		res.status(200).json({
 			message:"Usuario creado correctamente",
@@ -97,11 +104,26 @@ const userUpdate = (async (req = request,res = response) => {
 			});
 		}
 		else{
-		const updatedUser = await UserModel.findByIdAndUpdate(id,req.body,{new:true});
-		res.status(200).json({
-			message:"Usuario actualizado correctamente",
-			data: updatedUser
-		});
+			
+		
+		 // Check if username exists in the database
+		 const existingUser = await UserModel.findOne({ Username: req.body.username });
+		
+		 
+		// Username exists, proceed with user update
+		 if (existingUser) {
+			const {Username,Password,Name,Lastname,Email,Enabled}= req.body;
+			let _user = new UserModel({_id:id,Username,Password,Name,Lastname,Email,Enabled});
+			const updatedUser = await UserModel.findByIdAndUpdate(id,_user,{new:true});
+			res.status(200).json({
+				message:"Usuario actualizado correctamente",
+				data: updatedUser
+			});
+		 }
+		 else{
+			return res.status(409).send({ message: "Usuario no existe" });
+		 }
+
 		}
 		//Alternative Test simple response
 		//res.send('Function to update users');
@@ -121,7 +143,7 @@ const userDelete = (async (req = request,res = response) => {
 			});
 		}
 		else{
-		const updatedUser = await UserModel.findByIdAndDelete(id,req.body,{new:true});
+		const updatedUser = await UserModel.findByIdAndDelete(id,null,{new:true});
 		res.status(200).json({
 			message:"Usuario eliminado correctamente",
 			data: updatedUser
