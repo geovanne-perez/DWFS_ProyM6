61f2674d6d3fececf210a76e6cf20ac8e4f849a4
Geovanne Perez <geovanne.perez@outlook.com>
2024-05-30 16:37:03 -0600

    Formateo de archivos y agregada función para consultar carritos activos de un usuario

--- routes/shoppingCarts.Router.js
+++ routes/shoppingCarts.Router.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const authMidd = require('../middleware/authorization')
+const express = require("express");
+const router = express.Router();
+const authMidd = require("../middleware/authorization");
 
 const {
-    GetCart,
-    AddToCart,
-    CheckoutCart
-} = require ('../controllers/shoppingCarts.Controller');
-
+  GetCart,
+  GetActiveCarts,
+  AddToCart,
+  CheckoutCart,
+  CancelCart,
+} = require("../controllers/shoppingCarts.Controller");
 
 // Swagger Schema definition
 /**
@@ -26,10 +27,9 @@ const {
  *        carId: UID
  */
 
-
 /**
  * @swagger
- *  /api/cart/:  
+ *  /api/cart/:
  *  get:
  *      summary: Obtener un carrito de compras
  *      tags: [cart]
@@ -52,11 +52,38 @@ const {
  *          500:
  *              description: Error interno de servidor
  */
-router.get('/',authMidd,GetCart);
+router.get("/", authMidd, GetCart);
 
 /**
  * @swagger
- *  /api/cart/:  
+ *  /api/cart/userId:
+ *  get:
+ *      summary: Obtener todos los carritos activos de un usuario
+ *      tags: [cart]
+ *      parameters:
+ *        - in: query
+ *          name: id
+ *          schema:
+ *              type: string
+ *          required: true
+ *          description: User Id
+ *      security:
+ *          - Authorization: []
+ *      responses:
+ *          200:
+ *              description: Carritos de compras encontrados
+ *          400:
+ *              description: Id de carrito requerido
+ *          409:
+ *              description: Carrito no encontrado en catálogo o no disponible
+ *          500:
+ *              description: Error interno de servidor
+ */
+router.get("/userId", authMidd, GetActiveCarts);
+
+/**
+ * @swagger
+ *  /api/cart/:
  *  post:
  *      summary: Alta de nuevo producto en carrito
  *      tags: [cart]
@@ -82,12 +109,11 @@ router.get('/',authMidd,GetCart);
  *          500:
  *              description: Error interno de servidor
  */
-router.post('/',authMidd,AddToCart);
-
+router.post("/", authMidd, AddToCart);
 
 /**
  * @swagger
- *  /api/cart/:  
+ *  /api/cart/:
  *  put:
  *      summary: Completar compra de carrito
  *      tags: [cart]
@@ -105,7 +131,7 @@ router.post('/',authMidd,AddToCart);
  *              description: Producto comprado exitosamente
  *              content:
  *                  application/json:
-*                       schema:
+ *                       schema:
  *                          $ref: '#/components/schemas/cart'
  *          400:
  *              description: Id de carrito requerido
@@ -114,6 +140,37 @@ router.post('/',authMidd,AddToCart);
  *          500:
  *              description: Error interno de servidor
  */
-router.put('/',authMidd,CheckoutCart);
+router.put("/", authMidd, CheckoutCart);
+
+/**
+ * @swagger
+ *  /api/cart/:
+ *  delete:
+ *      summary: Cancelar compra de auto
+ *      tags: [cart]
+ *      parameters:
+ *        - in: query
+ *          name: id
+ *          schema:
+ *              type: string
+ *          required: true
+ *          description: Cart Id
+ *      security:
+ *          - Authorization: []
+ *      responses:
+ *          200:
+ *              description: Carrito cancelado exitosamente
+ *              content:
+ *                  application/json:
+ *                       schema:
+ *                          $ref: '#/components/schemas/cart'
+ *          400:
+ *              description: Id de carrito requerido
+ *          409:
+ *              description: Carrito o Auto no encontrado o Auto no disponible
+ *          500:
+ *              description: Error interno de servidor
+ */
+router.delete("/", authMidd, CancelCart);
 
 module.exports = router;
